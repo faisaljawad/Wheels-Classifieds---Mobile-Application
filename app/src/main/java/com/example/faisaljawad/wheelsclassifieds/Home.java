230cf44d825f42014c4abff72e97418681fe8b25
Faisal Jawad <faisaljawad1@gmail.com>
2018-11-30 00:25:34 +0500

    Added overflow menu and Background

--- app/src/main/java/com/example/faisaljawad/wheelsclassifieds/Home.java
+++ app/src/main/java/com/example/faisaljawad/wheelsclassifieds/Home.java
@@ -2,6 +2,9 @@ package com.example.faisaljawad.wheelsclassifieds;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
     }
 }
