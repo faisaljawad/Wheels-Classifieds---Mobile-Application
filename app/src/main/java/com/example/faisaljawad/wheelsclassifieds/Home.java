252cd23d7e697382de3ad70b4f877df051f744b3
Junaid Ahmed <junaidkol09@gmail.com>
2018-12-23 17:15:15 +0500

    Splash Screen issue resolved

--- app/src/main/java/com/example/faisaljawad/wheelsclassifieds/Home.java
+++ app/src/main/java/com/example/faisaljawad/wheelsclassifieds/Home.java
@@ -1,6 +1,7 @@
 package com.example.faisaljawad.wheelsclassifieds;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,34 +13,21 @@ import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
 
+    private static int splash_time_out = 1000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splash = new Intent(Home.this,SplashScreen.class);
+                startActivity(splash);
+                finish();
+            }
+        },splash_time_out);
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.Signup)
-        {
-            Toast.makeText(this, "Signup Activity", Toast.LENGTH_SHORT).show();
-            Intent i = new Intent(this, SignUp.class);
-            startActivity(i);
-        }
-        if (item.getItemId() == R.id.Login)
-        {
-            Toast.makeText(this, "Login Activity", Toast.LENGTH_SHORT).show();
-            Intent i = new Intent(this, Login.class);
-            startActivity(i);
-        }
-        if(item.getItemId()==R.id.help)
-            Toast.makeText(this, "Help Activity", Toast.LENGTH_SHORT).show();
-        if(item.getItemId()==R.id.about)
-            Toast.makeText(this, "About Activity", Toast.LENGTH_SHORT).show();
-            return super.onOptionsItemSelected(item);
-    }
+
 }
--- app/src/main/java/com/example/faisaljawad/wheelsclassifieds/SplashScreen.java
+++ app/src/main/java/com/example/faisaljawad/wheelsclassifieds/SplashScreen.java
@@ -4,23 +4,40 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity {
-
-    private static int splash_time_out = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent splash = new Intent(SplashScreen.this,Home.class);
-                startActivity(splash);
-                finish();
-            }
-        },2000);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.Signup)
+        {
+            Toast.makeText(this, "Signup Activity", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(this, SignUp.class);
+            startActivity(i);
+        }
+        if (item.getItemId() == R.id.Login)
+        {
+            Toast.makeText(this, "Login Activity", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(this, Login.class);
+            startActivity(i);
+        }
+        if(item.getItemId()==R.id.help)
+            Toast.makeText(this, "Help Activity", Toast.LENGTH_SHORT).show();
+        if(item.getItemId()==R.id.about)
+            Toast.makeText(this, "About Activity", Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 }
