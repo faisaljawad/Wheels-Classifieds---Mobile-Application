fda30c5ca3cf09515371b04a0e83ea99ea47a40f
Sana Anjum <sanaaaanjum22@gmail.com>
2018-12-25 21:31:49 +0500

    Fixing problem in Contact us Page

--- app/src/main/java/com/example/faisaljawad/wheelsclassifieds/ContactUs_Activity.java
+++ app/src/main/java/com/example/faisaljawad/wheelsclassifieds/ContactUs_Activity.java
@@ -8,24 +8,20 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class ContactUs_Activity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class ContactUs_Activity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us_);
         Spinner spinner=findViewById(R.id.spinnercontactus);
-        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.Subject,android.R.layout.simple_spinner_item);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(ContactUs_Activity.this, android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.Subject));
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-        spinner.setOnItemClickListener(this);
+
 
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        String text=parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
 
-    }
 }
