4a787f1796564b22279ed74addc6a60214edaaef
Faisal Jawad <faisaljawad1@gmail.com>
2018-12-30 23:46:44 +0500

    Forget Password page fixing

--- app/src/main/java/com/example/faisaljawad/wheelsclassifieds/forgetpassword.java
+++ app/src/main/java/com/example/faisaljawad/wheelsclassifieds/forgetpassword.java
@@ -16,46 +16,48 @@ import com.google.firebase.auth.FirebaseAuth;
 public class forgetpassword extends AppCompatActivity {
 
     private EditText pass_email;
-    private Button submitbtn;
-    private FirebaseAuth firebaseAuth; //firebase Auth is an object here
+    Button submit_btn;
+    FirebaseAuth firebaseAuth; //firebase Auth is an object here
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgetpassword);
         pass_email=findViewById(R.id.emailforgetpassword);
-         submitbtn=(Button)findViewById(R.id.btnForgetPassword);
-         firebaseAuth=FirebaseAuth.getInstance();
-         submitbtn.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 String useremail=pass_email.getText().toString().trim(); //getting txt and converting into string and trim is used to remove whitespaces
-                 if(useremail.equals(""))//if user email is null display this msg
-                 {
-                     Toast.makeText(forgetpassword.this,"Please enter your registered Email ID.", Toast.LENGTH_SHORT).show();
-                 }
-                 else
-                 {
-                     firebaseAuth.sendPasswordResetEmail(useremail).addOnCompleteListener(new OnCompleteListener<Void>() {
-                         @Override
-                         public void onComplete(@NonNull Task<Void> task) {
-                             if(task.isSuccessful())
-                             {
-                                 Toast.makeText(forgetpassword.this,"Password reset email sent.", Toast.LENGTH_SHORT).show();
-                                 finish();
-                                 startActivity(new Intent(forgetpassword.this, Login.class));
-                             }
-                             else
-                             {
-                                 Toast.makeText(forgetpassword.this,"Error in sending mail.", Toast.LENGTH_SHORT).show();
-                             }
-                         }
-                     });
-                 }
-             }
-         });
-
-
+        //firebaseAuth=FirebaseAuth.getInstance();
+        submit_btn=(Button)findViewById(R.id.btnForgetPassword);
+        submit_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(forgetpassword.this, "Hello",Toast.LENGTH_LONG).show();
+                //start_forget_password();
+            }
+        });
+    }
+    public void start_forget_password()
+    {
+        String useremail=pass_email.getText().toString().trim(); //getting txt and converting into string and trim is used to remove whitespaces
+        if(useremail.equals(""))//if user email is null display this msg
+        {
+            Toast.makeText(forgetpassword.this,"Please enter your registered Email ID.", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            firebaseAuth.sendPasswordResetEmail(useremail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful())
+                    {
+                        Toast.makeText(forgetpassword.this,"Password reset email sent.", Toast.LENGTH_SHORT).show();
+                        //finish();
+                        //startActivity(new Intent(forgetpassword.this, Login.class));
+                    }
+                    else
+                    {
+                        Toast.makeText(forgetpassword.this,"Error in sending mail.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 
 }
