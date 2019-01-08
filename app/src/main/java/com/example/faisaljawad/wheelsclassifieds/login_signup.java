41a6c4b3a3cca2edb487101ebcbb2c534d70a766
mustaqeem7 <mustaqeem71110@ucp.edu.pk>
2019-01-08 20:17:06 +0500

    Login With facebook Done

--- app/src/main/java/com/example/faisaljawad/wheelsclassifieds/login_signup.java
+++ app/src/main/java/com/example/faisaljawad/wheelsclassifieds/login_signup.java
@@ -6,8 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -17,14 +26,22 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthCredential;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.Arrays;
 
 public class login_signup extends AppCompatActivity {
 
     FirebaseAuth authenticator;
     GoogleSignInClient mGoogleSignInClient;
+    CallbackManager callbackManager;
+
+    static int LoginType = -1;
+    LoginButton loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +49,11 @@ public class login_signup extends AppCompatActivity {
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
         mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
         authenticator = FirebaseAuth.getInstance();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton = (LoginButton) findViewById(R.id.btnLoginWithFacebook);
+        loginButton.setReadPermissions(Arrays.asList("email"));
     }
 
     public void Redirect_Login(View view)
@@ -46,10 +68,30 @@ public class login_signup extends AppCompatActivity {
     }
     public void SignInUsingGoogle(View view)
     {
+        LoginType = 1;
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, 101);
     }
+    public void SignInUsingFacebook(View view)
+    {
+        LoginType = 2;
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookToken(loginResult.getAccessToken());
+            }
 
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(),"Sign In was Canceled",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(getApplicationContext(),error.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 
     private void firebaseAuthWithGoogle(GoogleSignInAccount account)
     {
@@ -63,7 +105,7 @@ public class login_signup extends AppCompatActivity {
                             FirebaseUser user = authenticator.getCurrentUser();
                             Intent i = new Intent(getApplicationContext(),Login.class);
                             startActivity(i);
-                            Toast.makeText(getApplicationContext(),"Sign IN Successful",Toast.LENGTH_LONG).show();
+                            Toast.makeText(getApplicationContext(),"Sign in Successful",Toast.LENGTH_LONG).show();
                             finish();
                         }
                         else {
@@ -77,18 +119,37 @@ public class login_signup extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == 101) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                firebaseAuthWithGoogle(account);
-            } catch (ApiException e) {
+        if (LoginType == 1) {                       // if google button clicked only than this would work
+            if (requestCode == 101) {
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                try {
+                    GoogleSignInAccount account = task.getResult(ApiException.class);
+                    firebaseAuthWithGoogle(account);
+                } catch (ApiException e) {
 
+                }
             }
+        }
+        else if (LoginType == 2) {                       // if Facebook button clicked only than this would work
+            callbackManager.onActivityResult(requestCode, resultCode, data);
         }
     }
 
-
-
+    private void handleFacebookToken(AccessToken accessToken) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        authenticator.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful())
+                {
+                    FirebaseUser fbuser = authenticator.getCurrentUser();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Sign in was Unsuccessful",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 
 }
