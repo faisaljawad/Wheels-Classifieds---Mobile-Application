5812558adf70a6ebee4eb4ee4184867c78404c81
mustaqeem7 <mustaqeem71110@ucp.edu.pk>
2019-01-25 18:21:07 +0500

    List View Resources Fixed

--- app/src/main/java/com/example/faisaljawad/wheelsclassifieds/login_signup.java
+++ app/src/main/java/com/example/faisaljawad/wheelsclassifieds/login_signup.java
@@ -46,7 +46,7 @@ public class login_signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_signup);
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken("482592555502-gt4u22cnd7hl5vaucvm90bkmi0q66oar.apps.googleusercontent.com").requestEmail().build();
         mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
         authenticator = FirebaseAuth.getInstance();
         FacebookSdk.sdkInitialize(getApplicationContext());
